23531798ec9c091fb8d9073a1ab64839a9340a04
imransayyed-scm <imransd6@gmail.com>
2025-05-29 15:42:46 +0530

    Delete Terraform-code/tf-rds-failover-automation directory

--- Terraform-code/tf-rds-failover-automation/main.tf
+++
@@ -1,4 +0,0 @@
-# tf-rds-failover-automation/main.tf
-
-# This file can be used for general module setup or left empty if resources are organized elsewhere.
-# For this module, resources are in iam.tf, lambda.tf, ssm.tf.
