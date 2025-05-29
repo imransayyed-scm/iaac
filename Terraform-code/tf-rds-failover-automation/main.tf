690ba0d4766d3e01c84ea20dc706a34424ecd192
imransayyed-scm <imransd6@gmail.com>
2025-05-29 15:36:22 +0530

    Create main.tf

---
+++ Terraform-code/tf-rds-failover-automation/main.tf
@@ -0,0 +1,4 @@
+# tf-rds-failover-automation/main.tf
+
+# This file can be used for general module setup or left empty if resources are organized elsewhere.
+# For this module, resources are in iam.tf, lambda.tf, ssm.tf.
